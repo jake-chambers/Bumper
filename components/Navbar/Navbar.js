fe0be76f0be4dd3487656982b12cbd52d35815e5
Jacob <jake_chambers12@hotmail.com>
2019-06-26 15:53:40 -0400

    nav

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
-import NavbarItem from '../NavbarItem/NavbarItem'
+import {Icon} from 'react-native-elements'
+import { TouchableOpacity } from 'react-native-gesture-handler';
 export default class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            settingsOpen: false,
+            profileOpen: false,
+            routesOpen: false
+        };
+    }
 
+    selectNavItem(){
+
+    }
     render() {
 
         return (
             <View style={styles.container}>
-                <NavbarItem name = "Profile"></NavbarItem>
-                <NavbarItem name = "Routes"></NavbarItem>
-                <NavbarItem name = "Settings"></NavbarItem>
+                <View style={styles.view}>
+                    <TouchableOpacity style={styles.item}>
+                        <Icon iconStyle={styles.icon} name = "user" type = "antdesign" size = {31}/>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.view}>
+                    <TouchableOpacity style={[styles.item,styles.middle]} class = 'middle'>
+                        <Icon iconStyle={styles.icon} name="map-pin" type="feather" size={33} />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.view}>
+                    <TouchableOpacity style={styles.item}>
+                        <Icon iconStyle = {styles.icon} name="settings" type="feather" size={33} />
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
@@ -21,9 +45,32 @@ const styles = StyleSheet.create({
         flex: 0.1,
         backgroundColor: 'gray',
         display: 'flex',
-        flexDirection: 'row', 
-        alignItems: 'center',
-        justifyContent: 'space-evenly'
-
+        flexDirection: 'row',
+        justifyContent: 'center'
     },
+
+    view: {
+        backgroundColor:'gray',
+        width: '33.333333%',
+        display: 'flex',
+        justifyContent: 'center'
+    },
+    item: {
+        backgroundColor: 'white',
+        display: 'flex',
+        flexBasis: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    icon:{
+        position: 'relative', 
+        bottom: 5
+    },
+    middle: {
+        borderRightColor: 'gray',
+        borderLeftColor: 'gray',
+        borderLeftWidth: 1,
+        borderRightWidth: 1,
+    }
 });
--- components/NavbarItem/NavbarItem.js
+++ components/NavbarItem/NavbarItem.js
@@ -1,32 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import { Icon } from 'react-native-elements';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-const NavbarItem = (props) => (
-    <View style = {styles.position}>
-        <Text style={styles.words}>{props.name}</Text>
-    </View>
-);
 
-const styles = StyleSheet.create({
-    position: {
-        backgroundColor: 'white',
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-evenly',
-        flex: 1,
-        height: '100%',
-        borderStyle: 'solid',
-        borderColor: 'gray',
-        borderWidth: 1,
-    },
-
-    words: {
-        backgroundColor: 'white',
-        padding: '10%',
-        position: 'relative',
-
+export default class NavbarItem extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Main</Text>
+            </View>
+        );
     }
-});
-
-export default NavbarItem
+}
